c4deca82c66e1c4b7988bc862306ca396e8cb528
tuantm <147854704+tuantm204@users.noreply.github.com>
2024-11-27 09:05:44 +0700

    add src

---
+++ src/model/Account.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author truongmanhtuan
+ */
+public class Account {
+    private String userName;
+    private String password;
+
+    public Account(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+}
